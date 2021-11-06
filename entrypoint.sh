c349c31d41dfe9566cd6315e024ed87a41170652
TaYaKi71751 <56256195+TaYaKi71751@users.noreply.github.com>
2021-11-06 23:19:02 +0900

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -25,12 +25,10 @@ git lfs install
 # publish any new files
 git checkout ${BRANCH_NAME}
 git add -A
-TZPATH=$(find /usr/share/zoneinfo | grep "zoneinfo/${TIMEZONE}") 
-echo $TZPATH
+TZPATH=$(find /usr/share/zoneinfo | grep "zoneinfo/${TIMEZONE}")
 ln -s $TZPATH /etc/localtime
 ls -la /etc/localtime
 timestamp=$(date)
-echo $timestamp
 git commit -m "${TASK_NAME} ${timestamp} ${GITHUB_SHA}" || exit 0
 git pull --rebase publisher ${BRANCH_NAME}
 git push publisher ${BRANCH_NAME}
