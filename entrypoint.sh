c043ef37724ddc757bb580993ca811b2e5bd06f9
TaYaKi71751 <56256195+TaYaKi71751@users.noreply.github.com>
2021-11-12 17:19:29 +0900

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,7 +9,7 @@ fi
 if [ -z "${BRANCH_NAME}" ]; then
    BRANCH_NAME="$(git rev-parse --abbrev-ref HEAD)"
    readonly BRANCH_NAME
-   export BRANCH_NAME=$BRANCH_NAME
+#    export BRANCH_NAME=$BRANCH_NAME
    echo "\$BRANCH_NAME\=$BRANCH_NAME"
 fi
 
