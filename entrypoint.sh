2970aecdf16088f3b3e3776a4ba34f4bcf9ac2a4
TaYaKi71751 <56256195+TaYaKi71751@users.noreply.github.com>
2021-11-12 16:54:30 +0900

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -31,5 +31,5 @@ ln -s $TZPATH /etc/localtime
 ls -la /etc/localtime
 timestamp=$(date)
 git commit -m "${TASK_NAME} ${timestamp} ${GITHUB_SHA}" || exit 0
-git pull --rebase publisher ${BRANCH_NAME}
-git push publisher ${BRANCH_NAME}
+git pull --rebase publisher "${BRANCH_NAME}"
+git push publisher "${BRANCH_NAME}"
